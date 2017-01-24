e93a6c412571b4d5f0aef0a6882ca85e6944e118
letocollection <leonard@letocollection.com>
2017-01-23 17:39:04 -0800

    working on firebase

--- src/Bets.jsx
+++ src/Bets.jsx
@@ -1,28 +1,67 @@
-const React = require('react')
+import React, { Component } from 'react'
+import * as firebase from 'firebase';
 
-const Bets = React.createClass({
-  getInitialState () {
-    return {
-      player: '',
-      money: ''
+class Bets extends React.Component {
+
+  constructor(props, context){
+    super(props, context)
+    this.recordPlayer = this.recordPlayer.bind(this)
+    this.recordWager = this.recordWager.bind(this)
+    this.recordBet = this.recordBet.bind(this)
+    this.state = {
+      playerName: '',
+      playerWager: '',
+      activeBool: false
     }
-  },
-  handlePlayer (P) {
-    this.setState({ player: P.target.value })
-  },
-  wagerMoney (M) {
-    this.setState({ money: M.target.value })
-  },
+  }
+
+  /*componentDidMount(){
+    firebase.database().ref('currentBets/').on('value', (snapshot)=>{
+      const currentBets = snapshot.val()
+
+      if (activeBool === true){
+        this.setstate({
+          playerName: '',
+          playerWager: ''
+        })
+      }
+    })
+  }*/
+
+  recordPlayer(event) {
+    this.setState({
+      playerName: event.target.value
+    })
+  }  
+
+  recordWager(event) {
+    this.setState({
+      playerWager: event.target.value
+    })
+  }
+
+  recordBet(event) {
+    if (this.state.playerName.length > 0 && this.state.playerWager.length > 0) {
+      this.setState({
+        activeBool: true
+      })
+      console.log('this should say true' + this.state.activeBool)
+    } else { console.log ('this shoudl say false' + this.state.activeBool) }
+  }
+
   render () {
     return (
       <div className='bets'>
-        <input value={this.state.player} className='search-input' type='text' placeholder='Player' onChange={this.handlePlayer} />
-        <input value={this.state.money} className='search-input' type='text' placeholder='Wager' onChange={this.wagerMoney} />
-          <h3>Player: {this.state.player}</h3>
-          <h3>$ {this.state.money} Wagered</h3>
+        <input value={this.state.playerName} className='search-input' type='text' placeholder='Player' onChange={this.recordPlayer} />
+        <input value={this.state.playerWager} className='search-input' type='text' placeholder='Wager' onChange={this.recordWager} />
+          <h3>Player: {this.state.playerName}</h3>
+          <h3>$ {this.state.playerWager} Wagered</h3>
+          <button onClick={this.recordBet} className="button">Record Bet</button>
       </div>
     )
   }
-})
+
+}
+  
 
 module.exports = Bets
--- src/index.js
+++ src/index.js
@@ -4,26 +4,27 @@ const Landing = require('./Landing')
 const Search = require('./Search')
 const Bets = require('./Bets')
 const Database = require('./Database')
+const Chatroom = require('./Chatroom')
 const { Router, Route, hashHistory } = require('react-router')
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBkGSfuPzOuZVUolkcLApQNAnR2tIPYzaQ",
-    authDomain: "bookeyman-f68c9.firebaseapp.com",
-    databaseURL: "https://bookeyman-f68c9.firebaseio.com",
-    storageBucket: "bookeyman-f68c9.appspot.com",
-    messagingSenderId: "342171387632"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBkGSfuPzOuZVUolkcLApQNAnR2tIPYzaQ",
+  authDomain: "bookeyman-f68c9.firebaseapp.com",
+  databaseURL: "https://bookeyman-f68c9.firebaseio.com",
+  storageBucket: "bookeyman-f68c9.appspot.com",
+  messagingSenderId: "342171387632"
+};
+firebase.initializeApp(config);
+
 
 const App = () => (
-  
-
   <Router history={hashHistory}>
     <Route path='/' component={Landing} />
     <Route path='/search' component={Search} />
     <Route path='/bets' component={Bets} />
     <Route path='/database' component={Database} />
+    <Route path='/chat' component={Chatroom} />
   </Router>
 )
 
